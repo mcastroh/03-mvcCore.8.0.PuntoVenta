fe043f8e1e78ba08763b29a1f7fd6a391a236a35
mcastroh <m_castrohinostroza@hotmail.com>
2024-08-17 13:15:23 -0500

    Initial commit

---
+++ WebPuntoVenta/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebPuntoVenta.Data.Context;
+
+namespace WebPuntoVenta
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(Options =>
+            {
+                Options.UseSqlServer(configuration.GetConnectionString("CnSqlServer"));
+            });
+
+            //services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            //services.AddTransient(typeof(IGenericService<>), typeof(GenericService<>));
+
+            //services.AddScoped<ICorreoService, CorreoService>();
+            //services.AddScoped<IUtilidadesService, UtilidadesService>();
+            //services.AddScoped<IRolService, RolService>();
+            //services.AddScoped<IFireBaseService, FireBaseService>();
+            //services.AddScoped<IUsuarioService, UsuarioService>();
+            //services.AddScoped<IEmpresaService, EmpresaService>();
+            //services.AddScoped<IArticuloService, ArticuloService>();
+
+            ////services.AddScoped<IVentaRepository, VentaRepository>();
+            ////services.AddScoped<IVentaService, VentaService>();
+
+            //services.AddScoped<IOrdenCompraRepository, OrdenCompraRepository>();
+            //services.AddScoped<IOrdenCompraService, OrdenCompraService>();
+
+            //services.AddScoped<ITablasBaseService, TablasBaseService>();
+
+
+            return services;
+        }
+
+
+    }
+}
---
+++ WebPuntoVenta/Models/OcasionModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+#nullable disable
+namespace Mvc.Models
+{
+    public class OcasionModel : Model
+    {
+        [Display(Name = "Ocasión")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Ocasion { get; set; }
+    }
+}
